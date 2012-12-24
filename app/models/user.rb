19e3d2fdb84f5b4fcafa9284c279540350acd76b
chadfaling <chadfaling@gmail.com>
2012-12-24 18:36:48 -0500

    location 4056

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -24,5 +25,23 @@ class User < ActiveRecord::Base
   validates :password, :presence     => true,
                        :confirmation => true,
                        :length       => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+# Return true if the user's password matches the submitted password.
+def has_password?(submitted_password)
+  # compare encrypted_password with the encrypted version of 
+  # submitted_password
+end
+
+private
+
+  def encrypt_password
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt (string)
+    string # Only a temporary implementation!
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  encrypted_password :string(255)
 #
 
 require 'spec_helper'
@@ -104,6 +105,10 @@ describe User do
   
     it "should have an encrypted password attribute" do 
       @user.should respond_to(:encrypted_password)
+    end
+
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
     end
   end
 end
