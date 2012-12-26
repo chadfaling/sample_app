9e3b3f58cc3a215fe5b515e2197072bbf5a07cde
chadfaling <chadfaling@gmail.com>
2012-12-26 12:47:52 -0500

    location 4137 salted password working with rspec test

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
-
+require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :email, :name, :password, :password_confirmation
@@ -28,20 +29,26 @@ class User < ActiveRecord::Base
 
   before_save :encrypt_password
 
-# Return true if the user's password matches the submitted password.
-def has_password?(submitted_password)
-  # compare encrypted_password with the encrypted version of 
-  # submitted_password
-end
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password) 
+  end
 
 private
 
   def encrypt_password
+    self.salt = make_salt if new_record?
     self.encrypted_password = encrypt(password)
   end
 
-  def encrypt (string)
-    string # Only a temporary implementation!
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
   end
 
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
 end
