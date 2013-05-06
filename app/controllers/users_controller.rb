c422db713f5e5430e753aca7dacd664c763e2cd5
chadfaling <chadfaling@gmail.com>
2013-05-06 08:01:52 -0400

    location 6201, end of chapter 10 questions completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
+  before_filter :signed_in_user, :only => [:new, :create]
 
   def index
     @title = "All users"
@@ -47,8 +48,13 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
+    user = User.find(params[:id])
+    if current_user?(user)
+      flash[:error] = "Admin suicide warning: can't delete yourself"
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
+    end
     redirect_to users_path
   end
 
@@ -65,5 +71,12 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+
+    def signed_in_user
+      if signed_in?
+      flash[:info]="You're already signed in cant access this"
+      redirect_to(root_path)
+    end
     end
 end
